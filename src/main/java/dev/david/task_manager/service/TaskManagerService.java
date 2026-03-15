612c1d50326ec12b5cdcad1c312e2df0e86bd56e
david <davidmiro15@gmail.com>
2026-03-15 01:37:42 -0400

    Implement CRUD operations for tasks Add GET, POST, PUT, DELETE endpoints for tasks Task CRUD feature completed

--- src/main/java/dev/david/task_manager/service/TaskManagerService.java
+++ src/main/java/dev/david/task_manager/service/TaskManagerService.java
@@ -2,7 +2,13 @@ package dev.david.task_manager.service;
 
 import dev.david.task_manager.entity.Task;
 import dev.david.task_manager.repository.TaskManagerRepository;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 
@@ -13,5 +19,37 @@ public class TaskManagerService {
         this.taskManagerRepository = taskManagerRepository;
     }
 
+    public void save() {
+        taskManagerRepository.save(new Task());
+    }
 
+    public List<Task> getAllTasks() {
+        return taskManagerRepository.findAll();
+    }
+
+    public void findById(Long id) {
+        taskManagerRepository.findById(id);
+    }
+
+    public Task getTaskById(Long id) {
+        Optional<Task> taskOptional = taskManagerRepository.findById(id);
+        return taskManagerRepository.
+                findById(id).
+                orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+    }
+
+    public Task createTask(Task task) {
+        return taskManagerRepository.save(task);
+    }
+    public Task updateById(Long id, Task task) {
+        taskManagerRepository.findById(id)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+        return taskManagerRepository.save(task);
+    }
+
+    public void deleteTaskById(Long id) {
+        taskManagerRepository.findById(id)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+        taskManagerRepository.deleteById(id);
+    }
 }
